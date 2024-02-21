19f0627e4c7354a91b9e41f4a54c336064af4047
Sandra <saandreis.aramac@gmail.com>
2024-02-21 11:27:22 +0100

    Clase menuPrincipal

---
+++ menuPrincipal.java
@@ -0,0 +1,23 @@
+
+/**
+ * @author Sandra Macias Aragon
+ * @version 21.febrero.2024
+ **/
+
+package proyecto;
+
+/**
+ * Creacion clase 'menuPrincipal'
+ */
+public class menuPrincipal {
+
+	/**
+	 * Metodo principal de la clase 'menuPrincipal'
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
