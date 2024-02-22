86b4b16124c2c3fa924cfa147152415cca9f8917
Sandra <saandreis.aramac@gmail.com>
2024-02-22 09:55:16 +0100

    Desarrollo de codigo en clase Moto y Coche

--- Coche.java
+++ Coche.java
@@ -9,6 +9,65 @@ package proyecto;
 /**
  * Creacion de la clase 'Coche'
  */
-public class Coche {
+public class Coche extends Vehiculo {
+	
+	private String modelo;
+	
+	private String marca;
+
+	/**
+	 * Constructor con todos los parametros de la clase 'Coche'
+	 * @param nombreProp
+	 * @param matricula
+	 * @param kms
+	 * @param marca
+	 * @param modelo
+	 */
+	public Coche(String nombreProp, String matricula, int kms,String marca, String modelo) {
+		super(nombreProp, matricula, kms);
+
+		this.marca = marca;
+		
+		this.modelo = modelo;
+	}
+
+	/**
+	 * Metodo para conocer valor del parametro 'modelo'
+	 * @return modelo
+	 */
+	public String getModelo() {
+		return modelo;
+	}
+
+	/**
+	 * Metodo para establecer valor del parametro 'modelo'
+	 * @param modelo
+	 */
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	/**
+	 * Metodo para conocer valor del parametro 'marca'
+	 * @return marca
+	 */
+	public String getMarca() {
+		return marca;
+	}
+
+	/**
+	 * Metodo para establecer valor del parametro 'marca'
+	 * @param marca
+	 */
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	@Override
+	public String toString() {
+		return "Coche [modelo=" + modelo + ", marca=" + marca + "]";
+	}
+	
+	
 
 }
--- Moto.java
+++ Moto.java
@@ -7,8 +7,67 @@
 package proyecto;
 
 /**
- * Creacion de la clase 'Moto'
+ * Creacion de la clase 'Moto' que hereda de la clase 'Vehiculo'
  */
-public class Moto {
+public class Moto extends Vehiculo {
+	
+	private String modelo;
+	
+	private String marca; 
+
+	/**
+	 * Constructor con todos los parametros de la clase 'Moto'
+	 * @param nombreProp
+	 * @param matricula
+	 * @param kms
+	 * @param modelo
+	 * @param marca
+	 */
+	public Moto(String nombreProp, String matricula, int kms, String modelo, String marca) {
+		super(nombreProp, matricula, kms);
+		
+		this.marca = marca;
+		
+		this.modelo = modelo;
+	}
+
+	/**
+	 * Metodo para conocer valor del parametro 'modelo'
+	 * @return modelo
+	 */
+	public String getModelo() {
+		return modelo;
+	}
+
+	/**
+	 * Metodo para establecer valor del parametro 'modelo'
+	 * @param modelo
+	 */
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	/**
+	 * Metodo para conocer valor del parametro 'marca'
+	 * @return marca
+	 */
+	public String getMarca() {
+		return marca;
+	}
+
+	/**
+	 * Metodo para establecer valor del parametro 'marca'
+	 * @param marca
+	 */
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	@Override
+	public String toString() {
+		return "Moto [modelo=" + modelo + ", marca=" + marca + "]";
+	}
+	
+	
 
 }
